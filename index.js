5bf0b5e89da754a39b3f5e3642bf68112faccccf
Dave Hollerbach <dhollerbach@control4.com>
2020-03-04 10:51:59 -0700

    formatting js properly

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 //  Packages
-const core = require('@actions/core');
-const execSync = require('child_process').execSync;
+var core = require('@actions/core');
+var execSync = require('child_process').execSync;
 code = execSync('sudo npm install exeq --save');
-const exeq = require('exeq');
+var exeq = require('exeq');
 
-//  Environment Variables
-const ARGS = core.getInput('args');
-const AWS_ACCESS_KEY_ID = core.getInput('aws-access-key-id');
-const AWS_SECRET_ACCESS_KEY = core.getInput('aws-secret-access-key');
+//  Environment Vars
+var ARGS = core.getInput('args');
+var AWS_ACCESS_KEY_ID = core.getInput('aws-access-key-id');
+var AWS_SECRET_ACCESS_KEY = core.getInput('aws-secret-access-key');
+
+
+//  Exeq Vars
+var installDocker = exeq([
+  'echo Installing docker...',
+  'sudo apt-get install docker.io -y',
+  'sudo systemctl unmask docker',
+  'sudo systemctl start docker'
+]);
+
+var installServerlessAndPlugins = exeq([
+  'echo Installing Serverless and plugins...',
+  'sudo npm i serverless -g',
+  'sudo npm i serverless-python-requirements',
+  'sudo npm i serverless-plugin-canary-deployments'
+]);
+
+var runServerlessDeploy = exeq([
+  `echo Running sudo sls deploy ${ARGS}...`,
+  `sudo sls config credentials --provider aws --key ${AWS_ACCESS_KEY_ID} --secret ${AWS_SECRET_ACCESS_KEY} ${ARGS}`,
+  `sudo sls deploy ${ARGS}`
+]);
 
 
 //  Main function
 if (require.main === module) {
-  exeq(
-    'echo Installing docker...',
-    'sudo apt-get install docker.io -y',
-    'sudo systemctl unmask docker',
-    'sudo systemctl start docker',
-    'echo Installing Serverless and plugins...',
-    'sudo npm i serverless -g',
-    'sudo npm i serverless-python-requirements',
-    'sudo npm i serverless-plugin-canary-deployments',
-    `echo Running sudo sls deploy ${ARGS}...`,
-    `sudo sls config credentials --provider aws --key ${AWS_ACCESS_KEY_ID} --secret ${AWS_SECRET_ACCESS_KEY} ${ARGS}`,
-    `sudo sls deploy ${ARGS}`
-  );
+  installDocker.q.on('done', function() {});
+  installServerlessAndPlugins.q.on('done', function() {});
+  runServerlessDeploy.q.on('done', function() {});
 }
