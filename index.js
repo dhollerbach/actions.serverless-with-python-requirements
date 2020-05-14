06ee5128aa59810e2dae1fd57b1de9ed7dda20f8
dhollerbach <dave.hollerbach@gmail.com>
2020-05-14 13:45:12 -0600

    general cleanup

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function runServerlessDeploy() {
     `if [ ${process.env.AWS_ACCESS_KEY_ID} ] && [ ${process.env.AWS_SECRET_ACCESS_KEY} ]; then
       sls config credentials --provider aws --key ${process.env.AWS_ACCESS_KEY_ID} --secret ${process.env.AWS_SECRET_ACCESS_KEY} ${ARGS}
     fi`,
+    `env`,
     `sls deploy ${ARGS}`
   )
 }
