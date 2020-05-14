409afcaf9bb6fbcc5f0315c669bb5892908f7c4e
dhollerbach <dave.hollerbach@gmail.com>
2020-05-13 19:30:33 -0600

    still trying

--- index.js
+++ index.js
@@ -53,11 +53,11 @@ async function runServerlessDeploy() {
   await exeq(
     `echo Running sudo sls deploy ${ARGS}...`,
     `if [ ${SERVERLESS_ACCESS_KEY} != '' ]; then 
-      SERVERLESS_ACCESS_KEY=${SERVERLESS_ACCESS_KEY}
+      export SERVERLESS_ACCESS_KEY=${SERVERLESS_ACCESS_KEY}
+      echo "using serverless"
     else
       sudo sls config credentials --provider aws --key ${AWS_ACCESS_KEY_ID} --secret ${AWS_SECRET_ACCESS_KEY} ${ARGS}
-    fi
-    `,
+    fi`,
     `sudo sls deploy ${ARGS}`
   )
 }
