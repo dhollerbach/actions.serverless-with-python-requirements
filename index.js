80bb48fa7787f27cfbf3f37ea72554b39f7df249
dhollerbach <dave@hollerbach.io>
2025-02-23 16:25:32 -0700

    updates

--- index.js
+++ index.js
@@ -40,8 +40,9 @@ async function runServerlessDeploy() {
     // Serverless access key
     } else {
       console.log("Running Serverless deploy (serverless access key)")
+      process.env.SERVERLESS_ACCESS_KEY = inputs.SERVERLESS_ACCESS_KEY
       await exeq(
-        `SERVERLESS_ACCESS_KEY=${inputs.SERVERLESS_ACCESS_KEY} serverless deploy --verbose || echo "::error:: Serverless deploy failed"`
+        `serverless deploy --verbose || echo "::error:: Serverless deploy failed"`
       )
     }
     
